7962deab3040bf91c713d462c0006d2f38cc7c34
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-02-24 10:24:45 -0300

    feat: added challenges context

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import '../styles/global.css'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChallengesContext.Provider>
+      <Component {...pageProps} />
+   </ChallengesContext.Provider>
+    )
 }
 
-export default MyApp
+export default MyApp;
