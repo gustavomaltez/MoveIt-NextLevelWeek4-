266ff11e6986a9e4d8cb7985f3ca489f122f43ed
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-02-24 10:13:17 -0300

    style: added ChallengeBox styles

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -2,9 +2,10 @@ import Head from 'next/head';
 import { ExperienceBar } from '../components/ExperienceBar'
 import styles from '../styles/pages/Home.module.css';
 import { Profile } from '../components/Profile';
-import { CompletedChallenges } from '../CompletedChallenges';
+
 import { Countdown } from '../components/Countdown';
 import { ChallengeBox } from '../components/ChallengeBox';
+import { CompletedChallenges } from '../components/CompletedChallenges';
 
 export default function Home() {
   return (
