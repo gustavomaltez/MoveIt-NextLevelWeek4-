d195a64551f9dd3c46b934ab14a459165ba8dbe3
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-02-24 09:04:22 -0300

    style: added abandon countdown button styles

--- moveit-next/src/components/Countdown/index.tsx
+++ moveit-next/src/components/Countdown/index.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/components/Countdown.module.css';
 
 export function Countdown(){
     const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+    const [isActive, setIsActive] = useState(false);
 
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
@@ -12,16 +12,16 @@ export function Countdown(){
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountDown(){
-        setActive(true);
+        setIsActive(true);
     }
 
     useEffect(() => {
-        if(active && time > 0){
+        if(isActive && time > 0){
             setTimeout(() => {
                 setTime(time-1);
             }, 1000)
         }
-    }, [active,time])
+    }, [isActive,time])
     return (
         <div>
             <div className={styles.countdownContainer}>
@@ -36,13 +36,21 @@ export function Countdown(){
                 </div>
             </div>
 
+            {isActive ? (<button 
+                type="button" 
+                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                onClick={startCountDown}
+            >
+                Abandorar ciclo
+            </button>) : (
             <button 
                 type="button" 
                 className={styles.countdownButton}
                 onClick={startCountDown}
             >
                 Iniciar um ciclo
-            </button>
+            </button>)}
+            
         </div>
     );
 }
