d442513ece6a23efd635545808dfffb3288f9801
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-02-26 09:04:55 -0300

    feat: added cookie storage data

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -7,27 +7,46 @@ import { Countdown } from '../components/Countdown';
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { GetServerSideProps } from 'next';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number
+}
+
+export default function Home({level, currentExperience, challengesCompleted}) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Início | Move.it</title>
-      </Head>
-      <ExperienceBar/>
+    <ChallengesProvider level={level} currentExperience={currentExperience} challengesCompleted={challengesCompleted}>
+        <div className={styles.container}>
+          <Head>
+            <title>Início | Move.it</title>
+          </Head>
+          <ExperienceBar/>
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile/>
-            <CompletedChallenges/>
-            <Countdown/>
-          </div>
-          <div>
-            <ChallengeBox/>
-          </div>
-        </section>
-      </CountdownProvider>
+          <CountdownProvider>
+            <section>
+              <div>
+                <Profile/>
+                <CompletedChallenges/>
+                <Countdown/>
+              </div>
+              <div>
+                <ChallengeBox/>
+              </div>
+            </section>
+          </CountdownProvider>
 
-    </div>)
+        </div>
+    </ChallengesProvider>)
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+
+  return {
+    props: {level: Number(level), currentExperience: Number(currentExperience), challengesCompleted: Number(challengesCompleted)}
+  }
 }
